8f07c0f2440ab3496f50dd4fd0200a0d6ac17f16
Meijiro <jesse.miller-paulson@banno.com>
2014-04-02 19:18:29 -0500

    commit test

---
+++ marioai/src/marioManiacs/agents/neuralNetwork/NeuralNetwork.java
@@ -0,0 +1,19 @@
+package marioManiacs.agents.neuralNetwork;
+import marioManiacs.agents.neuralNetwork.*;
+
+/* Represents the neural network
+ * Contains an array of Input nodes, Hidden nodes and Output nodes
+ * It contains the methods that tell each node within the network to check the values and calculate whether or not to fire
+ */
+
+public class NeuralNetwork
+{
+	InputNode[] inputNodes;
+	HiddenNode[] hiddenNodes;
+	OutputNode[] outputNodes;
+	
+	public NeuralNetwork()  //add to this constructor so that GA team can create new networks
+	{
+		
+	}
+}
